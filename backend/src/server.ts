cf2488e9c13b9fb6068202fba925e7cae7a32778
Ilya Baranchikov <ilyabaran4ikov@gmail.com>
2024-01-02 12:35:26 +0300

    CORS and server error log

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,9 +10,18 @@ import { SocketController } from "./controllers/SocketController"
 const PORT: string | number = process.env.PORT || 8999
 const app = express()
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  return next()
+})
+
 const server = http.createServer(app)
 
 const webSocketServer = new WebSocketServer({ server })
 const socketController = new SocketController(webSocketServer)
+
+webSocketServer.on("error", (error) => {
+  console.error("WebSocket Server Error:", error)
+})
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
